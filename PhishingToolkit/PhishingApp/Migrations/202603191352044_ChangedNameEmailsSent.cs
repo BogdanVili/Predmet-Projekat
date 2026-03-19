264a6f6111e44b00999c0862ad590c2603a84e27
Bogdan <Bogdan@DESKTOP-068F6V4>
2026-03-19 14:58:16 +0100

    small fix

---
+++ PhishingToolkit/PhishingApp/Migrations/202603191352044_ChangedNameEmailsSent.cs
@@ -0,0 +1,20 @@
+﻿namespace PhishingApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedNameEmailsSent : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Websites", "EmailsSent", c => c.Int(nullable: false));
+            DropColumn("dbo.Websites", "SentEmails");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Websites", "SentEmails", c => c.Int(nullable: false));
+            DropColumn("dbo.Websites", "EmailsSent");
+        }
+    }
+}
